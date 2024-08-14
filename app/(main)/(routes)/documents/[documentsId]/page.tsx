01382cd0d0e1242596a1b471201ffd9b52b9423c
Juan Plata <jfplata@simba.co>
2024-08-14 09:11:55 -0500

    commit del capitulo Toolbar component (Icon, Title)

--- app/(main)/(routes)/documents/[documentsId]/page.tsx
+++ app/(main)/(routes)/documents/[documentsId]/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+"use client"
 
-function DocumentIdPage() {
+import { useQuery } from "convex/react"
+
+import { Id } from "@/convex/_generated/dataModel"
+import { api } from "@/convex/_generated/api"
+import Toolbar from "@/components/toolbar"
+
+interface DocumentIdPageProps {
+  params: {
+    documentsId: Id<"documents">
+  }
+}
+
+function DocumentIdPage({ params }: DocumentIdPageProps) {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentsId
+  })
+
+  if (document === undefined) {
+    return (
+      <div>
+        Loading...
+      </div>
+    )
+  }
+
+  if (document === null) {
+    return <div>Not Found</div>
+  }
+
   return (
-    <div>
-        DocumentId
+    <div className="pb-40">
+      <div className="h-[35vh]"/>
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
     </div>
   )
 }
